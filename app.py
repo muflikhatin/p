c9aed180f0b5071d90edbfc92d12c50fb0968c23
muflikhatin <115399427+muflikhatin@users.noreply.github.com>
2023-12-04 23:39:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ from term_frequency import main as term_frequency_page
 from K_Means import main as K_Means_page
 from topic_modelling import main as topic_modelling_page
 from lda_knn_kmeans import main as lda_knn_kmeans_page
-
+from closeness_centrality_crawling import main as closeness_centrality_crawling_page
+from closeness_centrality import main as closeness_centrality_page
 
 # Sidebar untuk navigasi
 selected_page = st.sidebar.selectbox(
     "Pilih Halaman:",
     ["Halaman Utama", "Halaman Crawling Data",
-     "Halaman Preprocessing Data", "Halaman Term Frequency","Halaman K Means","Halaman lda knn kmeans", "Halaman Topic Modelling"])
+     "Halaman Preprocessing Data", "Halaman Term Frequency","Halaman K Means","Halaman lda knn kmeans", "Halaman Topic Modelling", "Halaman closeness centrality crawling", "Halaman closeness centrality"])
 
 # Menampilkan konten halaman yang dipilih
 if selected_page == "Halaman Utama":
@@ -26,9 +27,11 @@ elif selected_page == "Halaman Term Frequency":
     term_frequency_page()
 elif selected_page == "Halaman K Means":
     K_Means_page()
-# elif selected_page == "Halaman lda knn":
-#     lda_knn_page()
-elif selected_page == "Halaman lda knn kmeans":
-    lda_knn_kmeans_page()
+elif selected_page == "Halaman lda knn":
+    lda_knn_page()
+elif selected_page == "Halaman closeness centrality crawling":
+    closeness_centrality_crawling_page()
+elif selected_page == "Halaman closeness centrality":
+    closeness_centrality_page()
 else:
     topic_modelling_page()
