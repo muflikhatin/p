f8582ff879a167cb3303f2d155904da271126ccb
muflikhatin <115399427+muflikhatin@users.noreply.github.com>
2023-12-11 16:11:41 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -9,12 +9,13 @@ from topic_modelling import main as topic_modelling_page
 from lda_knn_kmeans import main as lda_knn_kmeans_page
 from closeness_centrality_crawling import main as closeness_centrality_crawling_page
 from closeness_centrality import main as closeness_centrality_page
+from klasivikasi_SVM import main as klasivikasi_SVM_page
 
 # Sidebar untuk navigasi
 selected_page = st.sidebar.selectbox(
     "Pilih Halaman:",
     ["Halaman Utama", "Halaman Crawling Data",
-     "Halaman Preprocessing Data", "Halaman Term Frequency","Halaman K Means","Halaman lda knn kmeans", "Halaman Topic Modelling", "Halaman closeness centrality crawling", "Halaman closeness centrality"])
+     "Halaman Preprocessing Data", "Halaman Term Frequency","Halaman K Means","Halaman lda knn kmeans", "Halaman Topic Modelling", "Halaman closeness centrality crawling", "Halaman closeness centrality", "Halaman Klasifikasi_SVM"])
 
 # Menampilkan konten halaman yang dipilih
 if selected_page == "Halaman Utama":
@@ -33,5 +34,7 @@ elif selected_page == "Halaman closeness centrality crawling":
     closeness_centrality_crawling_page()
 elif selected_page == "Halaman closeness centrality":
     closeness_centrality_page()
+elif selected_page == "Halaman Klasifikasi_SVM":
+    klasivikasi_SVM_page()    
 else:
     topic_modelling_page()
