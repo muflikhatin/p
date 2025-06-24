c53e9b4c4af3e7d87bd9b7edc625dc9c6d719b8d
muflikhatin <115399427+muflikhatin@users.noreply.github.com>
2025-06-24 19:44:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,22 @@
-# app.py
 import streamlit as st
 from main import main as main_page
-from crowling_data import main as crawling_data_page
-from preprocessing_data import main as preprocessing_data_page
-from term_frequency import main as term_frequency_page
-from K_Means import main as K_Means_page
-from topic_modelling import main as topic_modelling_page
-from lda_knn_kmeans import main as lda_knn_kmeans_page
-from closeness_centrality_crawling import main as closeness_centrality_crawling_page
-from closeness_centrality import main as closeness_centrality_page
-from klasivikasi_SVM import main as klasivikasi_SVM_page
+from crawling_berita import crawling_berita_page
+from preprocessing import preprocessing_page
+from cbow import cbow_page
+from bilstm import bilstm_page
 
-# Sidebar untuk navigasi
 selected_page = st.sidebar.selectbox(
     "Pilih Halaman:",
-    ["Halaman Utama", "Halaman Crawling Data",
-     "Halaman Preprocessing Data", "Halaman Term Frequency","Halaman K Means","Halaman lda knn kmeans", "Halaman Topic Modelling", "Halaman closeness centrality crawling", "Halaman closeness centrality", "Halaman Klasifikasi_SVM"])
+    ["Halaman Utama", "Halaman crawling_berita", "Halaman preprocessing", "Halaman CBOW", "Halaman BILSTM"]
+)
 
-# Menampilkan konten halaman yang dipilih
 if selected_page == "Halaman Utama":
     main_page()
-elif selected_page == "Halaman Crawling Data":
-    crawling_data_page()
-elif selected_page == "Halaman Preprocessing Data":
-    preprocessing_data_page()
-elif selected_page == "Halaman Term Frequency":
-    term_frequency_page()
-elif selected_page == "Halaman K Means":
-    K_Means_page()
-elif selected_page == "Halaman lda knn":
-    lda_knn_page()
-elif selected_page == "Halaman closeness centrality crawling":
-    closeness_centrality_crawling_page()
-elif selected_page == "Halaman closeness centrality":
-    closeness_centrality_page()
-elif selected_page == "Halaman Klasifikasi_SVM":
-    klasivikasi_SVM_page()    
-else:
-    topic_modelling_page()
+elif selected_page == "Halaman crawling_berita":
+    crawling_berita_page()
+elif selected_page == "Halaman preprocessing":
+    preprocessing_page()
+elif selected_page == "Halaman CBOW":
+    cbow_page()
+elif selected_page == "Halaman BILSTM":
+    bilstm_page()
